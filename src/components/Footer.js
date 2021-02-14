f6d65f8181cd99256a82cacdf6dad514d453f538
Alex Brashear <alexbrashear@gmail.com>
2021-02-14 08:57:42 -0500

    Modified footer to use Bootstrap

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,10 @@
 function Footer() {
-  const styles = {
-    footerStyles: {
-      paddingLeft: '5px',
-      background: '#5095E0',
-      color: 'white',
-      flexShrink: 0
-    }
-  };
   return (
-    <div style={styles.footerStyles}>
-      <h1>@2021 Alex Brashear</h1>
-    </div>
+    <nav className="navbar fixed-bottom navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">@2021 Alex Brashear</a>
+      </div>
+    </nav>
   );
 }
 
