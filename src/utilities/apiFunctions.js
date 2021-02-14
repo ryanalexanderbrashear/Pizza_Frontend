c0c7cb8f27b6e5dcecf6645dfaa0deb233b07837
Alex Brashear <alexbrashear@gmail.com>
2021-02-14 14:56:44 -0500

    Added functions for calling API to update and delete pizza

--- src/utilities/apiFunctions.js
+++ src/utilities/apiFunctions.js
@@ -4,9 +4,7 @@ export const retrievePeople = async () => {
   try {
     const response = await axios.get('http://localhost:9292/api/v1/people');
     if (response.status === 200 && response.data !== null) {
-      return response.data.map((datum) => {
-        return datum.name;
-      });
+      return response.data;
     } else {
       return [];
     }
@@ -20,9 +18,7 @@ export const retrievePizzas = async () => {
   try {
     const response = await axios.get('http://localhost:9292/api/v1/pizza');
     if (response.status === 200 && response.data !== null) {
-      return response.data.map((datum) => {
-        return datum.meat_type;
-      });
+      return response.data;
     } else {
       return [];
     }
@@ -35,8 +31,35 @@ export const retrievePizzas = async () => {
 export const createPizza = async (meatType) => {
   try {
     const response = await axios.post(`http://localhost:9292/api/v1/pizza?meat_type=${meatType}`);
-    console.log(response.status);
     if (response.status === 201) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
+export const updatePizza = async (id, meatType) => {
+  try {
+    const response = await axios.put(`http://localhost:9292/api/v1/pizza?id=${id}&meat_type=${meatType}`);
+    if (response.status === 200) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
+export const deletePizza = async (id) => {
+  try {
+    const response = await axios.delete(`http://localhost:9292/api/v1/pizza?id=${id}`);
+    if (response.status === 200) {
       return true;
     } else {
       return false;
