998b14275c0d0f319cde723a4036db9e62cae797
Alex Brashear <alexbrashear@gmail.com>
2021-02-14 08:52:50 -0500

    Modified header content to utilize Bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,10 @@
 function Header() {
-  const styles = {
-    headerStyles: {
-      padding: '30px',
-      textAlign: 'center',
-      background: '#5095E0',
-      color: 'white',
-      fontSize: '30px'
-    }
-  };
   return (
-    <div style={styles.headerStyles}>
-      <h1>Pizza Info Center</h1>
-    </div>
+    <nav className='navbar navbar-dark bg-dark'>
+      <div className='container-fluid'>
+        <a className='navbar-brand' href='#'>Pizza Analytics</a>
+      </div>
+    </nav>
   );
 }
 
