6ba0989a3ff4c42720a0aded424c3bfe02cc7fc3
Alex Brashear <alexbrashear@gmail.com>
2021-02-14 16:04:44 -0500

    Created the consumption table for displaying consumption records

---
+++ src/components/ConsumptionTable.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function ConsumptionTable(props) {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">Record ID</th>
+          <th scope="col">Name</th>
+          <th scope="col">Pizza Type</th>
+          <th scope="col">Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.list.map((record) => {
+          return (
+            <tr>
+              <td>{record.id}</td>
+              <td>{record.name}</td>
+              <td>{record.meat_type}</td>
+              <td>{record.date}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+export default ConsumptionTable;
