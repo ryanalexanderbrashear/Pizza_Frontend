0966eb7cdc1e6a4204738fc245b0d1270094caae
Alex Brashear <alexbrashear@gmail.com>
2021-02-14 10:06:15 -0500

    Added function to retrieve pizza list

--- src/utilities/apiFunctions.js
+++ src/utilities/apiFunctions.js
@@ -15,3 +15,19 @@ export const retrievePeople = async () => {
     return [];
   }
 }
+
+export const retrievePizza = async () => {
+  try {
+    const response = await axios.get('http://localhost:9292/api/v1/pizza');
+    if (response.status === 200 && response.data !== null) {
+      return response.data.map((datum) => {
+        return datum.meat_type;
+      });
+    } else {
+      return [];
+    }
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
