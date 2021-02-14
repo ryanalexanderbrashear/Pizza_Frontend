d00bf51ab6e977a4196b5f0c5b7ab358372c796c
Alex Brashear <alexbrashear@gmail.com>
2021-02-14 10:00:09 -0500

    Modified retrievePeople to be an async function to work better with state updates in components

--- src/utilities/apiFunctions.js
+++ src/utilities/apiFunctions.js
@@ -1,11 +1,17 @@
 import axios from 'axios';
 
-export const retrievePeople = () => {
-  axios.get('http://localhost:9292/api/v1/people')
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
+export const retrievePeople = async () => {
+  try {
+    const response = await axios.get('http://localhost:9292/api/v1/people');
+    if (response.status === 200 && response.data !== null) {
+      return response.data.map((datum) => {
+        return datum.name;
+      });
+    } else {
+      return [];
+    }
+  } catch (error) {
     console.log(error);
-  })
+    return [];
+  }
 }
