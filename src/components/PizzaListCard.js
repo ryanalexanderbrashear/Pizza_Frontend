a4b5c4d44dafc0f0b5714dd8551be22d2a5b243e
Alex Brashear <alexbrashear@gmail.com>
2021-02-15 11:14:31 -0500

    Added error displays to pizza list card

--- src/components/PizzaListCard.js
+++ src/components/PizzaListCard.js
@@ -3,45 +3,60 @@ import { retrievePizzas, createPizza, updatePizza, deletePizza } from "../utilit
 import Card from "./Card";
 import CardButton from './CardButton';
 import CardButtonContainer from './CardButtonContainer';
+import ErrorText from './ErrorText';
 import PizzaList from './PizzaList';
 
 function PizzaListCard() {
 
   const [pizza, setPizza] = useState([]);
+  const [error, setError] = useState(null);
 
   const retrievePizzaList = () => {
+    setError(null);
     retrievePizzas()
       .then((pizza) => {
+        if (pizza.length === 0) {
+          setError('No pizzas have been retrieved.');
+          setPizza([]);
+          return
+        }
         setPizza(pizza);
       })
   }
 
   const addNewPizza = () => {
+    setError(null);
+    setPizza([]);
     let meatType = prompt('What type of meat does this pizza have?');
 
     if (meatType && meatType !== '') {
       createPizza(meatType)
         .then((success) => {
-          if (success) retrievePizzaList()
+          if (success) retrievePizzaList();
+          else setError('Unable to create pizza. Please try again.');
         })
     }
   }
 
   const updatePizzaInfo = (id) => {
+    setError(null);
     let meatType = prompt('What type of meat does this pizza have?');
 
     if (meatType && meatType !== '') {
       updatePizza(id, meatType)
         .then((success) => {
           if (success) retrievePizzaList()
+          else setError('Unable to update pizza. Please try again.');
         })
     }
   }
 
   const deletePizzaByID = (id) => {
+    setError(null);
     deletePizza(id)
       .then((success) => {
         if (success) retrievePizzaList()
+        else setError('Unable to delete pizza. Please try again.');
       })
   }
 
@@ -49,6 +64,7 @@ function PizzaListCard() {
     <Card>
       <div className='card-body'>
         <h5 className='card-title'>{'Pizza Types'}</h5>
+        <ErrorText error={error} />
         <PizzaList pizzas={pizza} updateFunction={updatePizzaInfo} deleteFunction={deletePizzaByID} />
       </div>
       <CardButtonContainer>
