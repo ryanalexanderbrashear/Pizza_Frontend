0e45cbc2c426a9b4eaff518c2853b8a189f0828f
Alex Brashear <alexbrashear@gmail.com>
2021-02-15 11:16:24 -0500

    Added error display to consumption list card

--- src/components/ConsumptionListCard.js
+++ src/components/ConsumptionListCard.js
@@ -4,26 +4,39 @@ import Card from "./Card";
 import CardButton from './CardButton';
 import CardButtonContainer from './CardButtonContainer';
 import ConsumptionTable from './ConsumptionTable';
+import ErrorText from './ErrorText';
 
 function ConsumptionListCard() {
 
   const [consumptionRecords, setConsumptionRecords] = useState([]);
   const [pizzaTypeInputText, setPizzaTypeInputText] = useState('');
+  const [error, setError] = useState(null);
 
   function handleChange(event) {
     setPizzaTypeInputText(event.target.value);
   }
 
   const retrieveRecords = () => {
+    setError(null);
     retrieveConsumption()
       .then((records) => {
+        if (records.length === 0) {
+          setConsumptionRecords([]);
+          setError('No consumption records were found.');
+          return;
+        }
         setConsumptionRecords(records);
       })
   }
 
   const retrieveRecordsByMeat = () => {
-    retrieveConsumptionByMeat(pizzaTypeInputText)
+    retrieveConsumptionByMeat(pizzaTypeInputText.toLowerCase())
       .then((records) => {
+        if (records.length === 0) {
+          setConsumptionRecords([]);
+          setError('No consumption records were found for that meat type.');
+          return;
+        }
         setConsumptionRecords(records);
       })
     setPizzaTypeInputText('');
@@ -33,6 +46,7 @@ function ConsumptionListCard() {
     <Card>
       <div className='card-body'>
         <h5 className='card-title'>{'Pizza Consumption'}</h5>
+        <ErrorText error={error} />
         <ConsumptionTable consumptionRecords={consumptionRecords} />
         <div className='row mx-5'>
           <input className='my-2' type='text' name='pizzaType' value={pizzaTypeInputText} onChange={handleChange} />
