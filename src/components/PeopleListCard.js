9d6883eddd02185d4bf7ec3a8803cdc8f5e38c01
Alex Brashear <alexbrashear@gmail.com>
2021-02-14 19:22:47 -0500

    Implemented search by name functionality to people list card component

--- src/components/PeopleListCard.js
+++ src/components/PeopleListCard.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { retrievePeople } from "../utilities/apiFunctions";
+import { retrievePeople, retrievePerson } from "../utilities/apiFunctions";
 import Card from "./Card";
 import PeopleList from './PeopleList';
 import CardButton from './CardButton';
@@ -8,6 +8,7 @@ import CardButtonContainer from './CardButtonContainer';
 function PeopleListCard() {
 
   const [people, setPeople] = useState([]);
+  const [personInputText, setPersonInputText] = useState('');
 
   const retrievePeopleList = () => {
     retrievePeople()
@@ -16,14 +17,30 @@ function PeopleListCard() {
       })
   }
 
+  const retrievePersonByName = () => {
+    retrievePerson(personInputText)
+      .then((person) => {
+        setPeople([person]);
+      })
+    setPersonInputText('');
+  }
+
+  function handleChange(event) {
+    setPersonInputText(event.target.value);
+  }
+
   return (
     <Card>
       <div className='card-body'>
         <h5 className='card-title'>{'People'}</h5>
         <PeopleList list={people} />
+        <div className='row mx-5'>
+          <input type='text' name='personName' value={personInputText} onChange={handleChange} />
+          <CardButton title={'Retrieve Person By Name'} onClick={retrievePersonByName} />
+        </div>
       </div>
       <CardButtonContainer>
-        <CardButton title={'Retrieve People'} onClick={retrievePeopleList} />
+        <CardButton title={'Retrieve All People'} onClick={retrievePeopleList} />
         <CardButton title={'Clear'} onClick={() => setPeople([])} />
       </CardButtonContainer>
     </Card>
